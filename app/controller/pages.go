eda0567d3e7bbd4f4d4e1fa5e2a3bf3b042908d9
Alexander <alexpospelov87@gmail.com>
2024-11-12 16:20:46 +0700

    Добавил html-разметку

--- app/controller/pages.go
+++ app/controller/pages.go
@@ -1,14 +1,26 @@
 package controller
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func StartPage(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	text := "Приевет инженерм ПТО)!"
-	//возвращаем простой текст
-	fmt.Fprint(rw, text)
+	//Указываем нужный путь к файлу
+	path := filepath.Join("public", "html", "startStaticPage.html")
+	// html-шаблон
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+	// вывод шаблона в браузер
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
 }
