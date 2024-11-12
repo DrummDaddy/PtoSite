9a0d7fe9ffa54b9e7f843b8a5421b6848b7ed25d
Alexander <alexpospelov87@gmail.com>
2024-11-12 21:57:55 +0700

    Добавил работу с внешними файлами

--- app/controller/users.go
+++ app/controller/users.go
@@ -2,23 +2,32 @@ package controller
 
 import (
 	"PTOSite/app/model"
-	"encoding/json"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	// Получаем список всех пользователей
+	//получаем список всех пользователей
 	users, err := model.GetAllUsers()
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
-	//возвращаем список клиенту в формате JSON
-	err = json.NewEncoder(rw).Encode(users)
+	//указываем путь к файлу с шаблоном
+	main := filepath.Join("public", "html", "usersDynamicPage.html")
+	common := filepath.Join("public", "html", "common.html")
+	//создаем html-шаблон
+	tmpl, err := template.ParseFiles(main, common)
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
+		return
+	}
+	//исполняем именованный шаблон "users", передавая туда массив со списком пользователей
+	err = tmpl.ExecuteTemplate(rw, "users", users)
+	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
